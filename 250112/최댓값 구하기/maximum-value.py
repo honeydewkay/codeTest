eddee643b7befd78f240db209f114c4cd49551db
honeydewkay <93977113+honeydewkay@users.noreply.github.com>
2025-01-12 22:06:17 +0900

    Title: 최댓값 구하기, Time: 75ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250112/최댓값 구하기/maximum-value.py
+++ 250112/최댓값 구하기/maximum-value.py
@@ -4,9 +4,9 @@ a2 = int(a1[0])
 a3 = int(a1[1]) 
 a4 = int(a1[2]) 
 
-if a2 > a3 and a2 > a4: 
+if a2 >= a3 and a2 >= a4: 
     print(a2)
-elif a3 > a4 and a3 > a2:
+elif a3 >= a4 and a3 >=ma2:
     print(a3)
-elif a4 > a3 and a4 > a2:
+elif a4 >= a3 and a4 >= a2:
     print(a4)
