743a7a4c87f04b9fe5521d4613a13db55ffe3349
honeydewkay <93977113+honeydewkay@users.noreply.github.com>
2025-01-12 21:03:52 +0900

    Title: 4번의 크기 비교, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250112/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,27 @@
+a = int(input())
+inp2 = input()
+inp2_arr = inp2.split()
+b = int(inp2_arr[0])
+c = int(inp2_arr[1])
+d = int(inp2_arr[2])
+e = int(inp2_arr[3])
+
+if a > b:
+    print(1)
+else:
+    print(0)
+
+if a > c:
+    print(1)
+else:
+    print(0)
+
+if a > d:
+    print(1)
+else:
+    print(0)   
+
+if a > e:
+    print(1)
+else:
+    print(0)
