d52b4850b36334635bc7afcce67c5c29762d1131
honeydewkay <93977113+honeydewkay@users.noreply.github.com>
2025-01-27 12:50:31 +0900

    Title: a/b 출력, Time: 102ms, Memory: 19MB, Status: Wrong Answer - CodeTree

--- 250127/a/b 출력/a-divide-b.py
+++ 250127/a/b 출력/a-divide-b.py
@@ -1,15 +1,7 @@
 from decimal import Decimal, getcontext
 
-i = input()
-arr = i.split()
-a = int(arr[0])
-b = int(arr[1])
-# c = (float)(a // b)
-# print(c)
-
 # 입력 받기
-# a = int(input())
-# b = int(input())
+a, b = map(int, input().split())
 
 # Decimal 연산 설정: 소수점 22자리까지 계산
 getcontext().prec = 22
