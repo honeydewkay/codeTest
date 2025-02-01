ab1ccc55be5e5f77a927a63f486cb6134d289be4
honeydewkay <93977113+honeydewkay@users.noreply.github.com>
2025-02-01 16:17:28 +0900

    Title: 공약수의 유무, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250201/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 250201/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -1,7 +1,7 @@
 sati = False
 a, b = map(int, input().split())
 for i in range(a, b+1):
-    if 1920 % i == 0 or 2880 % i == 0:
+    if 1920 % i == 0 and 2880 % i == 0: # 공약수니까 and
         sati = True
 if sati == True:
     print(1)
