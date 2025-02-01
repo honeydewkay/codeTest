ab5c54a9e039a2fdca0254b319dbce31864393fd
honeydewkay <93977113+honeydewkay@users.noreply.github.com>
2025-02-01 16:37:58 +0900

    Title: 모두 3의 배수, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250201/모두 3의 배수/all-multiples-of-3.py
+++ 250201/모두 3의 배수/all-multiples-of-3.py
@@ -1,9 +1,9 @@
-sati = False
+sati = True
 for i in range(5):
     n = int(input())
-    if n % 3 == 0:
-        sati = True
-if sati == True:
-    print(1)
-else:
+    if n % 3 != 0:
+        sati = False
+if sati == False:
     print(0)
+else:
+    print(1)
