bebe11112cb8cce37c7b4fe8a71f37440a6f3f8d
honeydewkay <93977113+honeydewkay@users.noreply.github.com>
2025-02-01 16:28:30 +0900

    Title: 소수 판별기, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250201/소수 판별기/prime-number-discriminator.py
+++ 250201/소수 판별기/prime-number-discriminator.py
@@ -1,10 +1,10 @@
-sati = False
+sati = True
 n = int(input())
 
 for i in range(2, n):
     if n % i == 0:
-        sati = False
-if sati == True:
-    print("P")
-else:
+        sati = False # 소수가 아님
+if sati == False: # 소수가 아님
     print("C")
+else:
+    print("P")
