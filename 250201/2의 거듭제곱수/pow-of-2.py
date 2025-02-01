36adefe2e0a7db6eb1364160a9642c645c491ee4
honeydewkay <93977113+honeydewkay@users.noreply.github.com>
2025-02-01 15:23:47 +0900

    Title: 2의 거듭제곱수, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250201/2의 거듭제곱수/pow-of-2.py
+++ 250201/2의 거듭제곱수/pow-of-2.py
@@ -1,12 +1,13 @@
 n = int(input())
 x = 0
 while True:
-    if n != 1:
-        x += 1
-        n //= 2
     if n == 2:
         x += 1
         break
+    if n != 1:
+        x += 1
+        n = n //2
+        continue
     
 print(x)
 
