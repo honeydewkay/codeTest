eb5aab356c179ca0ade71bb98ac95cb00f7a554b
honeydewkay <93977113+honeydewkay@users.noreply.github.com>
2025-02-22 14:42:06 +0900

    Title: 구구단 만들기 2, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250222/구구단 만들기 2/print-multiplication-table-2.py
+++ 250222/구구단 만들기 2/print-multiplication-table-2.py
@@ -2,9 +2,12 @@ a, b = map(int, input().split())
 n = 2
 for i in range(b):
     for j in range(b, a-1, -1):
-        if j < b-1:
+        if j == a:
             print(f"{j} * {n} = {j*n}", end=" ")
         else:
             print(f"{j} * {n} = {j*n}", end=" / " )
-    n += 2
-    print()
+    if n <= 6:
+        n += 2
+    else:
+        break
+    print() 
