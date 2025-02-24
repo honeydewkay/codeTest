ebba9a064290e12abb6dd2dfd02386d046198e8c
honeydewkay <93977113+honeydewkay@users.noreply.github.com>
2025-02-24 21:42:43 +0900

    Title: 19단 출력, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250224/19단 출력/nineteen-times-table.py
@@ -0,0 +1,8 @@
+# Write your code here!
+for i in range(1, 20):
+    for j in range(1,20):
+        if j % 2 == 0 or j % 19 == 0:
+            print(f"{i} * {j} = {i*j}")
+        else:
+            print(f"{i} * {j} = {i*j}", end=" / ")
+            
